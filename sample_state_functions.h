c40a61f405cd902e2d0dbba7e494c8232adc5b88
joshkanna <joshuakannappilly10@gmail.com>
2024-12-03 17:57:41 -0500

    first commit

--- sample_state_functions.h
+++ sample_state_functions.h
@@ -1,16 +1,34 @@
+// sample_state_functions.h
+
 #include "i2c_utils.h"
 #include <stdio.h>
 #include <string.h>
 
+
 void idle(){
     // idle
+    printf("State: Idle\n");
 }
+
+void deploy(){
+
+}
+
 void detumbling(){
+    printf("State: Detumbling\n");
     // Need:
     // Sensors
     // Magnotorquers
     // Flight Computer
 }
+
+void deploySolar(){
+    printf("Deploying Solar Panels\n");
+    if (state_of_charge < 5){
+        current_state = sleep; // Critical battery condition
+    }
+}
+
 void idleWithSun(){
     // Need:
     // IMU
@@ -44,12 +62,16 @@ void uplinkDownlink(){
     // Sidekiq Z2
 }
 void lowChargeSunPointing(){
+    if (state_of_charge < 5){
+        current_state = sleep; // Critical battery condition
+    }
     // Need:
     // IMU
     // Magnetorquers
     // Flight Computer
     // Motors
 }
+   
 void sleep(){
     // Need:
     //honk shoo
--- state_machine.c
+++ state_machine.c
@@ -1,8 +1,10 @@
- #include<stdio.h>
+#include<stdio.h>
+#include "sample_state_functions.h"
 
 typedef void (*stateFuncPtr)(void);
 
 typedef enum transit_event{ 
+    EVT_NO_EVENT,
     LAUNCH_COMPLETE_EVT,
     DETUMBLING_COMPLETE_EVT,
     DEPLOYMENT_COMPLETE_EVT,
@@ -14,59 +16,80 @@ typedef enum transit_event{
     NOT_DOING_RESEARCH_EVT
 } transit_event;
 
-
+void idle(void);
 void detumbling(void);
+void deploySolar(void);
+void lowChargeSunPointing(void);
+void uplinkDownlink(void);
 void payload(void);
-void deploy(void);
+void sleep(void);
 
 typedef struct fc_stm {
     stateFuncPtr curr_state;
     transit_event trig_event;
     stateFuncPtr next_state;
+void deploy(void);
+void detumbling(void);
+void payload(void);
 
-} fc_stm;
+} fc_stm_row;
 
-fc_stm = {
-}
+static eEvent eCurrentEvent = EVT_NO_EVENT;
+static stateFuncPtr stCurrentState = idle;
+// Used to flag first entry to a state
+static bool bIsStateFirstEntry = false;
+// Used to flag an imminent exit from the state
+static bool bIsStateAboutToExit = false;
 
-void deploy(){
+fc_stm_row fc_stm[] = {
+    {idle, LAUNCH_COMPLETE_EVT, detumbling},
+    {detumbling, DETUMBLING_COMPLETE_EVT, deploySolar},
+    {deploySolar, DEPLOYMENT_COMPLETE_EVT, lowChargeSunPointing},
+    
+    // low charge state transitions
+    {lowChargeSunPointing, CRIT_BATTERY_EVT, sleep},
+    {lowChargeSunPointing, LOW_CHARGE_EVT, lowChargeSunPointing},
 
-}
+    // Battery charge check
+    {lowChargeSunPointing, ABOVE_COMMS_EVT, uplinkDownlink},
+    {uplinkDownlink, NOT_ABOVE_COMMS_EVT, payload},
 
-void detumbling(){
-    // Need:
-    // Sensors
-    // Magnotorquers
-    // Flight Computer
-}
-void payload(){
-    // Need:
-    // IMU
-    // Magnetorquers
-    // Payload
-    // Flight Computer
-    // Motors CONDITIONALLY
-}
-void uplinkDownlink(){
-    // Need:
-    // IMU
-    // Magnetorquers
-    // Flight Computer
-    // Motors
-    // Sidekiq Z2
-}
-void lowChargeSunPointing(){
-    // Need:
-    // IMU
-    // Magnetorquers
-    // Flight Computer
-    // Motors
-}
-void sleep(){
-    // Need:
-    //honk shoo
-}
+    // Payload operations
+    {payload, DOING_RESEARCH_EVT, payload}
+    {payload, NOT_DOING_RESEARCH_EVT, lowChargeSunPointing},
 
+    // Hibernate transitions
+    {sleep, LOW_CHARGE_EVT, lowChargeSunPointing}
+};
+
+// variables for state machine
+int state_of_charge = 100;
+int battery_temp = 0;
+bool above_comms;
+bool doing_research;
+stateFuncPtr current_state = idle;
+
+
+void StateMachineHandler()
+{
+    int idx = 0;
+    if(EVT_NO_EVENT != eCurrentEvent)
+    {
+        for(;idx < sizeof(fc_stm)/sizeof(fc_stm); idx++)
+        {
+            if( (fc_stm[idx].curr_state == stCurrentState) &&
+                (fc_stm[idx].trig_event == eCurrentEvent))
+            {
+                /// Clear event 
+                eCurrentEvent = EVT_NO_EVENT;
+                /// Notify the state that exit is imminent;
+                bIsStateAboutToExit = true;
+                stCurrentState
+
+            }
+        }
+    }
+}
 
 
 
