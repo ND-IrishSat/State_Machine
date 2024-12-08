3636589b9af1fba928339be64675f86fcaa2cfb3
BeegScoop <bfclashman15@gmail.com>
2024-12-08 01:06:58 -0500

    error fixed

--- i2c_utils.c
+++ i2c_utils.c
@@ -3,11 +3,6 @@
 int i2c_fd;
 
 int read_register_data(int reg_addr, int length, unsigned char *data) {
-    i2c_fd = open("/dev/i2c-1", O_RDWR);
-    if (i2c_fd < 0) {
-        perror("Failed to open the i2c bus.\n");
-        return -1;
-    }
 
     if (ioctl(i2c_fd, I2C_SLAVE, DEVICE_ADDRESS) < 0) {
         perror("Failed to acquire bus access and/or talk to slave.\n");
@@ -25,11 +20,16 @@ int read_register_data(int reg_addr, int length, unsigned char *data) {
         perror("Failed to read from the i2c bus.\n");
         return -1;
     }
-    close(i2c_fd);
+    
     return 0;
 }
 
 float get_voltage() {
+    i2c_fd = open("/dev/i2c-1", O_RDWR);
+    if (i2c_fd < 0) {
+        perror("Failed to open the i2c bus.\n");
+        return -1;
+    }
     unsigned char data[2] = {0};
 
     if (read_register_data(0x3B, 2, data) != 0) {
@@ -37,5 +37,7 @@ float get_voltage() {
     }
 
     int raw_value = (data[0] << 8) | data[1];
+    close(i2c_fd);
     return raw_value * 0.001f; // Convert to volts (mV to V)
+
 }
