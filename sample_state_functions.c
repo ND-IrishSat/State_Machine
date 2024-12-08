bb8e521409d1240e7d2272f2648a86c41250cf56
BeegScoop <bfclashman15@gmail.com>
2024-12-08 10:59:07 -0500

    I stole josh's code

--- sample_state_functions.c
+++ sample_state_functions.c
@@ -1,33 +1,107 @@
-#include "i2c_utils.h"
+// sample_state_functions.h
+
 #include <stdio.h>
 #include <string.h>
 
+void trigger_event(transit_event event){
+    eCurrentEvent = event;
+}
+
 void idle(){
     // idle
+    printf("State: Idle\n");
 }
+
 void detumbling(){
+    if(bIsStateFirstEntry)
+    {
+        /// Do stuff for first entry(state initialization stuff)
+        printf("State: Detumbling\n");
+        trigger_event(DETUMBLING_COMPLETE_EVT);   // Critical battery condition
+    }
+
+    if(bIsStateAboutToExit)
+    {
+        /// Handle exit(i.e free up resources)
+        printf("Exiting Detumbling.");
+    }
+
     // Need:
     // Sensors
     // Magnotorquers
     // Flight Computer
 }
-void idleWithSun(){
-    // Need:
-    // IMU
-    // Magnetorquers
-    // Flight Computer
-    // Solar Panel DONE ONCE
-    // Antenna Deployment DONE ONCE
 
+void deploySolar(){
+    if(bIsStateFirstEntry)
+    {
+        /// Do stuff for first entry(state initialization stuff)
+        printf("State: Deploying Solar Panels\n");
+        trigger_event(DEPLOYMENT_COMPLETE_EVT);   // Critical battery condition
+    }
+
+    if(bIsStateAboutToExit)
+    {
+        /// Handle exit(i.e free up resources)
+        printf("Exiting Deploy Solar");
+    }
 }
-void idleWithoutSun(){
+
+void lowChargeSunPointing(){
+    if(bIsStateFirstEntry)
+    {
+        /// Do stuff for first entry(state initialization stuff)
+        printf("State: Low Charge Sun Pointing\n");
+        if (state_of_charge < 5){
+            trigger_event(CRIT_BATTERY_EVT);   // Critical battery condition
+        } 
+        else if (battery_temp < 0){
+            // heat up
+            battery_temp = 1;
+        } 
+        else if (state_of_charge < 30) {
+            trigger_event(LOW_CHARGE_EVT);
+        } 
+        else if (above_comms){
+            trigger_event(ABOVE_COMMS_EVT);
+        }
+        else if (doing_research){
+            trigger_event(DOING_RESEARCH_EVT);
+        }
+    }
+
+    if(bIsStateAboutToExit)
+    {
+        /// Handle exit(i.e free up resources)
+        printf("Exiting Low Charge Sun Pointing.");
+    }
+
     // Need:
     // IMU
     // Magnetorquers
     // Flight Computer
-    // Heat CONDITIONALLY
+    // Motors
 }
+
 void payload(){
+    if(bIsStateFirstEntry)
+    {
+        /// Do stuff for first entry(state initialization stuff)
+        printf("State: Payload\n");
+        if (doing_research) {
+            trigger_event(DOING_RESEARCH_EVT);
+        }
+        else {
+            trigger_event(NOT_DOING_RESEARCH_EVT);
+        }
+    }
+
+    if(bIsStateAboutToExit)
+    {
+        /// Handle exit(i.e free up resources)
+        printf("Exiting Payload");
+    }
+
     // Need:
     // IMU
     // Magnetorquers
@@ -35,22 +109,35 @@ void payload(){
     // Flight Computer
     // Motors CONDITIONALLY
 }
+
 void uplinkDownlink(){
-    // Need:
-    // IMU
-    // Magnetorquers
-    // Flight Computer
-    // Motors
-    // Sidekiq Z2
+    if(bIsStateFirstEntry)
+    {
+        // Do stuff for first entry(state initialization stuff)
+        printf("State: Down/Up link\n");
+        if (doing_research) {
+            trigger_event(DOING_RESEARCH_EVT);
+        }
+        else {
+            trigger_event(LOW_CHARGE_EVT);
+        }
+    }
+
+    if(bIsStateAboutToExit)
+    {
+        /// Handle exit(i.e free up resources)
+        printf("Exiting Down/Up link");
+    }
 }
-void lowChargeSunPointing(){
-    // Need:
-    // IMU
-    // Magnetorquers
-    // Flight Computer
-    // Motors
-}
-void sleep(){
+   
+void hibernate(){
     // Need:
     //honk shoo
+
+    if (state_of_charge > 5) {
+        transit_event(LOW_CHARGE_EVT);
+    }
+    else {
+        transit_event(CRIT_BATTERY_EVT);
+    }
 }
--- state_machine.c
+++ state_machine.c
@@ -1,13 +1,18 @@
-#include<stdio.h>
+
+#include <stdbool.h>
+#include <stdio.h>
+#include "sample_state_functions.h"
 #include "i2c_utils.h"
 
 typedef void (*stateFuncPtr)(void);
 
+// Define all events
 typedef enum transit_event{ 
-    NO_EVT,
+    EVT_NO_EVENT,
     LAUNCH_COMPLETE_EVT,
     DETUMBLING_COMPLETE_EVT,
     DEPLOYMENT_COMPLETE_EVT,
+    
     CRIT_BATTERY_EVT,
     LOW_CHARGE_EVT,
     ABOVE_COMMS_EVT,
@@ -17,70 +22,99 @@ typedef enum transit_event{
 } transit_event;
 
 
+// Define all states
+void idle(void);
 void detumbling(void);
+void deploySolar(void);
+void lowChargeSunPointing(void);
+void uplinkDownlink(void);
 void payload(void);
-void deploy(void);
-void low_charge_sun_pointing(void);
 void hibernate(void);
-void payload(void);
-void uplink_downlink(void);
 
+// Define a row of STM
 typedef struct fc_stm {
     stateFuncPtr curr_state;
     transit_event trig_event;
     stateFuncPtr next_state;
+} fc_stm_row;
 
-} fc_stm;
+static transit_event eCurrentEvent = EVT_NO_EVENT;
+static stateFuncPtr stCurrentState = idle;
+// Used to flag first entry to a state
+static bool bIsStateFirstEntry = false;
+// Used to flag an imminent exit from the state
+static bool bIsStateAboutToExit = false;
 
-void launching(void){
+fc_stm_row fc_stm[] = {
+    {idle, LAUNCH_COMPLETE_EVT, detumbling},
+    {detumbling, DETUMBLING_COMPLETE_EVT, deploySolar},
+    {deploySolar, DEPLOYMENT_COMPLETE_EVT, lowChargeSunPointing},
+    
+    // low charge state transitions
+    {lowChargeSunPointing, CRIT_BATTERY_EVT, hibernate},
+    {lowChargeSunPointing, LOW_CHARGE_EVT, lowChargeSunPointing},
+    {lowChargeSunPointing, ABOVE_COMMS_EVT, uplinkDownlink},
+    {lowChargeSunPointing, NOT_ABOVE_COMMS_EVT, lowChargeSunPointing},
+    {lowChargeSunPointing, DOING_RESEARCH_EVT, payload},
+    {lowChargeSunPointing, NOT_DOING_RESEARCH_EVT, lowChargeSunPointing},
 
-}
+    // Down/Up link operations
+    {uplinkDownlink, DOING_RESEARCH_EVT, payload},
+    {uplinkDownlink, NOT_DOING_RESEARCH_EVT, lowChargeSunPointing},
 
-void detumbling(){
-    // Need:
-    // Sensors
-    // Magnotorquers
-    // Flight Computer
-}
+    // Payload operations
+    {payload, DOING_RESEARCH_EVT, payload},
+    {payload, NOT_DOING_RESEARCH_EVT, lowChargeSunPointing},
 
-void deploy(){
+    // Hibernate transitions
+    {hibernate, LOW_CHARGE_EVT, lowChargeSunPointing},
+    {hibernate, CRIT_BATTERY_EVT, hibernate}
+};
 
-}
-
-void low_charge_sun_pointing(){
-    // Need:
-    // IMU
-    // Magnetorquers
-    // Flight Computer
-    // Motors
-}
+// variables for state machine
+int state_of_charge = 100;
+int battery_temp = 0;
+bool above_comms;
+bool doing_research;
+stateFuncPtr current_state = idle;
 
 
-void hibernate(){
-    // Need:
-    //honk shoo
-}
+void StateMachineHandler()
+{
+    int idx = 0;
+    if(EVT_NO_EVENT != eCurrentEvent)
+    {
+        for(;idx < sizeof(fc_stm)/sizeof(fc_stm); idx++)
+        {
+            if( (fc_stm[idx].curr_state == stCurrentState) &&
+                (fc_stm[idx].trig_event == eCurrentEvent))
+            {
+                /// Clear event 
+                eCurrentEvent = EVT_NO_EVENT;
+                /// Notify the state that exit is imminent;
+                bIsStateAboutToExit = true;
+                stCurrentState();
+                // Move to new state
+                stCurrentState = stStm[idx].next_state;
+                // Flag that this is the first entry to the state
+                bIsStateFirstEntry = true
+            }
+        }
+    }
 
-void payload(){
-    // Need:
-    // IMU
-    // Magnetorquers
-    // Payload
-    // Flight Computer
-    // Motors CONDITIONALLY
-}
-void uplink_downlink(){
-    // Need:
-    // IMU
-    // Magnetorquers
-    // Flight Computer
-    // Motors
-    // Sidekiq Z2
-}
+    if(NULL != stCurrentState)
+    {
+        /// Run the state
+        stCurrentState();
+        /// Unflag first entry
+        bIsStateFirstEntry = false;
+    }
+};
 
-static transit_event curr_evt = NO_EVT
-
-fc_stm state_matrix = {
-    {launching, LAUNCH_COMPLETE_EVT, detumbling},
-
+int main()
+{
+    while(true)
+    {
+        StateMachineHadnler();
+    }
 };
